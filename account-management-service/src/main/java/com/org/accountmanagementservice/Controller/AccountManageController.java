69ee91d1391d9c4d3e1f10d0ab665ff8eb788489
shaikh shahzade <shahzade10@gmail.com>
2023-08-29 21:27:22 +0530

    Account management service : get User

--- account-management-service/src/main/java/com/org/accountmanagementservice/Controller/AccountManageController.java
+++ account-management-service/src/main/java/com/org/accountmanagementservice/Controller/AccountManageController.java
@@ -1,5 +1,6 @@
 package com.org.accountmanagementservice.Controller;
 
+import com.org.accountmanagementservice.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,12 @@ public class AccountManageController {
     public String getStatus()
     {
         return "Working";
+    }
+
+    @GetMapping("status")
+    public User getUser()
+    {
+        return new User("abcd","shaikh","shahzade","shahzade10@gmail.com");
     }
 
 }
---
+++ account-management-service/src/main/java/com/org/accountmanagementservice/model/User.java
@@ -0,0 +1,17 @@
+package com.org.accountmanagementservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String id;
+    private String fname;
+    private String lname;
+    private String email;
+}
