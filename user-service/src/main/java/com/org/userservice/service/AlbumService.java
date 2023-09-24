91c25aab6ad31ef4165f688bba088468af38efc3
shaikh shahzade <shahzade10@gmail.com>
2023-09-24 19:25:30 +0530

    Resillience4j: Retry implemented

--- user-service/src/main/java/com/org/userservice/service/AlbumService.java
+++ user-service/src/main/java/com/org/userservice/service/AlbumService.java
@@ -3,6 +3,7 @@ package com.org.userservice.service;
 import com.org.userservice.model.Album;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@ public interface AlbumService {
 
     @GetMapping("/users/1/albums")
     @CircuitBreaker(name="Photo-service-circuitbreaker",fallbackMethod = "getAlbumsFallback")
+    @Retry(name="Photo-service-circuitbreaker")
     public List<Album> getAlbums();
 
     public default List<Album> getAlbumsFallback(Exception e)
@@ -27,6 +29,7 @@ public interface AlbumService {
         Logger logger = LoggerFactory.getLogger(AlbumService.class);
         logger.info("Fallback method called for albums service call");
         logger.info(e.getMessage());
+        System.out.println(e);
         return new ArrayList<Album>();
 
     }
